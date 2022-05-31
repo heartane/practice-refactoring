9599553d2d48ed21dfc26e73349ce782cd4d3ead
Jin garam <2001garam@naver.com>
2022-05-31 16:28:15 +0900

    HTML 렌더 기능을 위한 처리 로직 위치 조정, statementData에 모든 데이터 담기

--- to-be/2.saperateSteps.js
+++ to-be/2.saperateSteps.js
@@ -7,6 +7,8 @@
 1. 데이터를 처리하는 단계
 2. 처리한 결과를 가지고 텍스트나 HTML로 표현하는 단계
 - 그 사이에 데이터 구조를 변환시켜 줄 조력자(매개) 필요 -> 중간 데이터
+1. 중간 매개자를 이용해서 내부 함수의 매개변수를 줄이자. 
+  - data에 모든 정보를 넣어 invoice, plays 변수를 없애자
 */
 
 import INVOICE from '../invoices.json';
@@ -16,71 +18,78 @@ import PLAYS from '../plays.json';
 function statement(invoice, plays) {
   const statementData = {}; // 중간 매개자
   statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances;
-  return renderPlainText(statementData, plays);
+  statementData.performances = invoice.performances.map(extendPerformance);
+  // 인자가 동일하면 생략 가능
+  return renderPlainText(statementData); // 모든 정보를 다 넣은 매개변수
+
+  // 기존 데이터 처리 로직을 모두 이쪽으로!
+  function extendPerformance(performance) {
+    const result = { ...performance }; // 얕은 복사 -> 모든 정보를 담을 버킷이 만들어졌다!
+    result.play = getPlayInfo(performance); // play 정보를 함께 버무려!
+    result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
+    console.log('불리기', result);
+    return result;
+  }
+
+  // 공연 정보 조회 함수
+  function getPlayInfo(performance) {
+    return plays[performance.playID];
+  }
+
+  // 공연에 대한 요금 책정 함수
+  function amountFor(performance) {
+    let result = 0;
+    console.log(performance);
+    switch (performance.play.type) {
+      case 'tragedy':
+        result = 40000;
+        if (performance.audience > 30)
+          result += 1000 * (performance.audience - 30);
+        break;
+      case 'comedy':
+        result = 30000;
+        if (performance.audience > 20)
+          result += 1000 + 500 * (performance.audience - 20);
+        result += 300 * performance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${performance.play.type}`);
+    }
+    return result;
+  }
+
+  // 공연 포인트 적립 함수
+  function volumeCreditsFor(performance) {
+    let result = 0;
+    result += Math.max(performance.audience - 30, 0);
+
+    // 희극은 관객 5명마다 추가 포인트 제공
+    if ('comedy' === performance.play.type)
+      result += Math.floor(performance.audience / 5);
+    return result;
+  }
 }
 
 // 공연료 청구서 계산 코드 -> 텍스트로 출력
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
   // 최상위 함수 statement와 매개변수가 다르다!
   let result = `청구 내역(고객명: ${data.customer})\n`; // 중간 매개자를 통해 데이터를 얻는다.
 
   for (let perf of data.performances) {
     // 청구 내역을 출력한다.
-    result += `${getPlayInfo(perf).name}: ${usd(amountFor(perf))} (${
-      perf.audience
-    }석)\n`;
+    result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
   }
 
   result += `총액: ${usd(totalAmount())}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 
-  // 공연에 대한 요금 책정 함수
-  function amountFor(performanceInfo) {
-    let result = 0;
-
-    switch (getPlayInfo(performanceInfo).type) {
-      case 'tragedy':
-        result = 40000;
-        if (performanceInfo.audience > 30)
-          result += 1000 * (performanceInfo.audience - 30);
-        break;
-      case 'comedy':
-        result = 30000;
-        if (performanceInfo.audience > 20)
-          result += 1000 + 500 * (performanceInfo.audience - 20);
-        result += 300 * performanceInfo.audience;
-        break;
-      default:
-        throw new Error(
-          `알 수 없는 장르: ${getPlayInfo(performanceInfo).type}`
-        );
-    }
-    return result;
-  }
-
-  // 공연 정보 조회 함수
-  function getPlayInfo(performanceInfo) {
-    return plays[performanceInfo.playID];
-  }
-
-  // 공연 포인트 적립 함수
-  function volumeCreditsFor(performanceInfo) {
-    let result = 0;
-    result += Math.max(performanceInfo.audience - 30, 0);
-
-    // 희극은 관객 5명마다 추가 포인트 제공
-    if ('comedy' === getPlayInfo(performanceInfo).type)
-      result += Math.floor(performanceInfo.audience / 5);
-    return result;
-  }
-
   // 총 적립 포인트 계산 함수
   function totalVolumeCredits() {
     let result = 0;
     for (let perf of data.performances) {
-      result += volumeCreditsFor(perf);
+      result += perf.volumeCredits;
     }
     return result;
   }
@@ -89,7 +98,7 @@ function renderPlainText(data, plays) {
   function totalAmount() {
     let result = 0;
     for (let perf of data.performances) {
-      result += amountFor(perf);
+      result += perf.amount;
     }
     return result;
   }
