fc1ec006fa9ab87df1b3676ad23180627b760e35
Jin garam <2001garam@naver.com>
2022-06-01 12:07:25 +0900

    조건부 로직을 다형성으로 바꾸기, 공연 비용처리 함수를 클래스 메소드로 이전

--- to-be/4.polymorphism/createStatementData.js
+++ to-be/4.polymorphism/createStatementData.js
@@ -16,6 +16,37 @@ class PerformanceCalculator {
     this.performance = performance;
     this.play = play;
   }
+  // 공연에 대한 요금 책정 함수 -> 다형성 적용 요함
+  get amount() {
+    let result = 0;
+
+    switch (this.play.type) {
+      case 'tragedy':
+        result = 40000;
+        if (this.performance.audience > 30)
+          result += 1000 * (this.performance.audience - 30);
+        break;
+      case 'comedy':
+        result = 30000;
+        if (this.performance.audience > 20)
+          result += 1000 + 500 * (this.performance.audience - 20);
+        result += 300 * this.performance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${this.play.type}`);
+    }
+    return result;
+  }
+
+  // 공연 포인트 적립 함수 -> 다형성 적용 요함
+  get volumeCredits() {
+    let result = 0;
+    result += Math.max(this.performance.audience - 30, 0);
+
+    if ('comedy' === this.play.type)
+      result += Math.floor(this.performance.audience / 5);
+    return result;
+  }
 }
 
 export default function createStatementData(invoice, plays) {
@@ -36,8 +67,8 @@ export default function createStatementData(invoice, plays) {
     console.log(calculator);
     const result = { ...performance };
     result.play = calculator.play;
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
+    result.amount = calculator.amount;
+    result.volumeCredits = calculator.volumeCredits;
     return result;
   }
 
@@ -48,34 +79,14 @@ export default function createStatementData(invoice, plays) {
 
   // 공연에 대한 요금 책정 함수 -> 다형성 적용 요함
   function amountFor(performance) {
-    let result = 0;
-
-    switch (performance.play.type) {
-      case 'tragedy':
-        result = 40000;
-        if (performance.audience > 30)
-          result += 1000 * (performance.audience - 30);
-        break;
-      case 'comedy':
-        result = 30000;
-        if (performance.audience > 20)
-          result += 1000 + 500 * (performance.audience - 20);
-        result += 300 * performance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${performance.play.type}`);
-    }
-    return result;
+    return new PerformanceCalculator(performance, getPlayInfo(performance))
+      .amount;
   }
 
   // 공연 포인트 적립 함수 -> 다형성 적용 요함
   function volumeCreditsFor(performance) {
-    let result = 0;
-    result += Math.max(performance.audience - 30, 0);
-
-    if ('comedy' === performance.play.type)
-      result += Math.floor(performance.audience / 5);
-    return result;
+    return new PerformanceCalculator(performance, getPlayInfo(performance))
+      .volumeCredits;
   }
 
   // 총 적립 포인트 계산 함수
