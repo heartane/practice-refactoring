ad5893ae12cea52ff99827e9c7fecc7e16a2af4c
Jin garam <2001garam@naver.com>
2022-06-01 12:42:44 +0900

    팩토리 메소드로 다형성 지원, 하위 클래스 생성 및 오버라이딩
    - 명확한 매개변수 파악을 위해 관사를 통한 단수복수 파악
    - PerformanceCalculator 호출 부분을 다형성 지원을 위해 팩토리
    메서드로 교체
    - 조건에 따라 하위 클래스 생성, comedy tragedy
    - 메소드 오버라이딩 후 확인

--- to-be/4.polymorphism/createStatementData.js
+++ to-be/4.polymorphism/createStatementData.js
@@ -7,25 +7,25 @@
 - 먼저 클래스로 계산하는 행위를 묶어보자
   - 필요한 속성(데이터)를 생성자 함수에 전달하기
   - 기존 함수들을 클래스 안으로 옮기기 (함수 옮기기)
-
+- 다형성 구조를 지원하는 팩토리 메소드를 만든다.
+- 조건에 따라 상위클래스를 상속하는 하위클래스를 만들자
+  - 상위 클래스 메소드에 조건문을 하위클래스에 적절히 오버라이딩한다.
+  - 하위 클래스를 생성하고 호출 시 적절한 클래스를 사용하면 된다.
 */
 
-// 각 공연에 비용처리에 대한 계산 클래스
+// 각 공연에 비용처리에 대한 계산 상위 클래스
 class PerformanceCalculator {
-  constructor(performance, play) {
-    this.performance = performance;
-    this.play = play;
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance;
+    this.play = aPlay;
   }
-  // 공연에 대한 요금 책정 함수 -> 다형성 적용 요함
+  // 공연에 대한 요금 책정 함수
   get amount() {
     let result = 0;
 
     switch (this.play.type) {
       case 'tragedy':
-        result = 40000;
-        if (this.performance.audience > 30)
-          result += 1000 * (this.performance.audience - 30);
-        break;
+        throw '오류발생';
       case 'comedy':
         result = 30000;
         if (this.performance.audience > 20)
@@ -38,7 +38,7 @@ class PerformanceCalculator {
     return result;
   }
 
-  // 공연 포인트 적립 함수 -> 다형성 적용 요함
+  // 공연 포인트 적립 함수
   get volumeCredits() {
     let result = 0;
     result += Math.max(this.performance.audience - 30, 0);
@@ -48,9 +48,33 @@ class PerformanceCalculator {
     return result;
   }
 }
+// 상속하는 하위 클래스
+class TragedyCalculator extends PerformanceCalculator {
+  // 오버라이딩
+  get amount() {
+    let result = 0;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+}
+class ComedyCalculator extends PerformanceCalculator {}
+
+// 팩토릴 메소드로 조건에 따른 인스턴스 생성 -> 다형성 적용 완료
+function createPerformanceCalculator(aPerformance, aPlay) {
+  switch (aPlay.type) {
+    case 'comedy':
+      return new ComedyCalculator(aPerformance, aPlay);
+    case 'tragedy':
+      return new TragedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+  }
+}
 
 export default function createStatementData(invoice, plays) {
-  const result = {}; // 중간 매개자 버킷
+  const result = {};
   result.customer = invoice.customer;
   result.performances = invoice.performances.map(extendPerformance);
   result.totalAmount = totalAmount(result);
@@ -59,13 +83,13 @@ export default function createStatementData(invoice, plays) {
   return result;
 
   // 한 공연에 필요한 데이터 처리 로직
-  function extendPerformance(performance) {
-    const calculator = new PerformanceCalculator(
-      performance,
-      getPlayInfo(performance)
-    );
+  function extendPerformance(aPerformance) {
+    const calculator = createPerformanceCalculator(
+      aPerformance,
+      getPlayInfo(aPerformance)
+    ); // 클래스로 인스턴스 생성 대신, 팩토리 함수 적용
     console.log(calculator);
-    const result = { ...performance };
+    const result = { ...aPerformance };
     result.play = calculator.play;
     result.amount = calculator.amount;
     result.volumeCredits = calculator.volumeCredits;
@@ -73,19 +97,19 @@ export default function createStatementData(invoice, plays) {
   }
 
   // 공연 정보 조회 함수
-  function getPlayInfo(performance) {
-    return plays[performance.playID];
+  function getPlayInfo(aPerformance) {
+    return plays[aPerformance.playID];
   }
 
   // 공연에 대한 요금 책정 함수 -> 다형성 적용 요함
-  function amountFor(performance) {
-    return new PerformanceCalculator(performance, getPlayInfo(performance))
+  function amountFor(aPerformance) {
+    return new PerformanceCalculator(aPerformance, getPlayInfo(aPerformance))
       .amount;
   }
 
   // 공연 포인트 적립 함수 -> 다형성 적용 요함
-  function volumeCreditsFor(performance) {
-    return new PerformanceCalculator(performance, getPlayInfo(performance))
+  function volumeCreditsFor(aPerformance) {
+    return new PerformanceCalculator(aPerformance, getPlayInfo(aPerformance))
       .volumeCredits;
   }
 
