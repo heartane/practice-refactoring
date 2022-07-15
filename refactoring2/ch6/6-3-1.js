4f5342183ae8fef409e55c6fae6826fbc1f0d4bf
Jin garam <2001garam@naver.com>
2022-07-15 22:15:20 +0900

    [Refact] 변수 추출하기

--- refactoring2/ch6/6-3-1.js
+++ refactoring2/ch6/6-3-1.js
@@ -1,8 +1,14 @@
+/* 
+변수로 추출하기
+
+긴 표현식을 잘라서 변수로 해당 표현에 의미를 부여하면 이해를 더욱 도울 수 있다.
+이해하는데 문제가 없다면 짧은 변수를 지향하자. 하지만 정말 필요한 정보라면 필요!!
+*/
+
 export function price(order) {
-  // 가격(price) = 기본가격 - 수량할인 + 배송비
-  return (
-    order.quantity * order.itemPrice -
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-    Math.min(order.quantity * order.itemPrice * 0.1, 100)
-  );
+  const originalPrice = order.quantity * order.itemPrice;
+  const discount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  const delivery = Math.min(order.quantity * order.itemPrice * 0.1, 100);
+
+  return originalPrice - discount + delivery;
 }
