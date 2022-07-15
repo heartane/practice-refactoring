7e87ed30924debb19230ec0b5d51b7aedd9c1b64
Jin garam <2001garam@naver.com>
2022-07-15 21:45:55 +0900

    [Refact] 함수 인라인하기

--- refactoring2/ch6/6-2.js
+++ refactoring2/ch6/6-2.js
@@ -1,20 +1,22 @@
+/* 
+함수 인라인하기
+
+- 추출한 함수가 재사용되지않는다? 굳이 추출할 이유가 있을까?
+- 행위를 너무 잘게 나누면 불필요한 호출이 일어난다.
+
+언제 어떤 식으로 짜는가는 정해진 규칙이나 만능 정답은 없다.
+뭐가 좋은가는 사실 지난날의 나의 경험과 개발자의 직관에 따라 결정되게 된다.
+*/
+
 // 예제 1
 export function rating(driver) {
-  return moreThanFiveLateDeliveries(driver) ? 2 : 1;
-}
-
-function moreThanFiveLateDeliveries(dvr) {
-  return dvr.numberOfLateDeliveries > 5;
+  return driver.numberOfLateDeliveries > 5 ? 2 : 1;
 }
 
 // 예제 2
 function reportLines(customer) {
-  const lines = [];
-  gatherCustomerData(lines, customer);
-  return lines;
-}
-
-function gatherCustomerData(out, customer) {
-  out.push(['name', customer.name]);
-  out.push(['location', customer.location]);
+  const result = [];
+  result.push(['name', customer.name]);
+  result.push(['location', customer.location]);
+  return result;
 }
