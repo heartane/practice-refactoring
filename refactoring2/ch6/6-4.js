b683649faae9ae3ed3de3efaf09ceaf1333e507b
Jin garam <2001garam@naver.com>
2022-07-15 23:58:00 +0900

    [Refact] 변수 인라인하기

--- refactoring2/ch6/6-4.js
+++ refactoring2/ch6/6-4.js
@@ -1,4 +1,8 @@
+/* 
+변수 인라인하기
+- 한눈에 봐도 이해가 명확하고 간결하면 변수 추출이 필요할까?
+*/
+
 export function isDeliveryFree(anOrder) {
-  let basePrice = anOrder.basePrice;
-  return basePrice > 1000;
+  return anOrder.basePrice > 1000;
 }
