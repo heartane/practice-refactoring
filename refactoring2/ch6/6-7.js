5ea9811c9f8df830b4de548092441af32fa6fd52
Jin garam <2001garam@naver.com>
2022-07-15 23:59:23 +0900

    [Refact] 변수 이름 짓기

--- refactoring2/ch6/6-7.js
+++ refactoring2/ch6/6-7.js
@@ -1,6 +1,13 @@
-let a = height * width;
+/* 
+변수 이름 짓기
 
-const cpyNm = '애플';
+축약어는 지양하자!
+뭘 의미하는지 유추하는데 시간을 버리지 않도록 구체적으로 지어주자.
+*/
 
-let tpHd = '제목없음';
-let result = `<h1>${tpHd}</h1>`;
+let area = width * height;
+
+const companyName = '애플';
+
+let title = '제목없음';
+let result = `<h1>${title}</h1>`;
