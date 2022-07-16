96d7d79f62ced09ab80e42f3137f27894f7030c9
Jin garam <2001garam@naver.com>
2022-07-16 09:36:29 +0900

    [Refact] 매개변수 객체로 변경하기

--- refactoring2/ch6/6-8.js
+++ refactoring2/ch6/6-8.js
@@ -1,5 +1,20 @@
-export function readingsOutsideRange(station, min, max) {
-  return station.readings.filter((r) => r.temp < min || r.temp > max);
+/* 
+매개 변수 객체 만들기
+
+어떤 인자가 필요한지, 또 그 순서까지 고려할 사항이 많아지면서 호출이 고통스러워진다.
+매개변수는 최대 3개를 넘지않는게 좋다.
+
+연관있는 데이터 구조 혹은 클래스 하나로 묶어주는 것이 좋다.
+*/
+
+export function doLotsOfThings(a, b, c, e, f) {
+  // do something...💩
+}
+
+export function readingsOutsideRange(station, range) {
+  return station.readings.filter(
+    (r) => r.temp < range.temperatureFloor || r.temp > range.temperatureCeiling
+  );
 }
 
 const station = {
@@ -12,13 +27,11 @@ const station = {
     { temp: 51, time: '2016-11-10 09:50' },
   ],
 };
+
+// 순수 데이터 객체
 const operationPlan = {
   temperatureFloor: 51,
   temperatureCeiling: 53,
 };
 
-readingsOutsideRange(
-  station,
-  operationPlan.temperatureFloor,
-  operationPlan.temperatureCeiling
-);
+readingsOutsideRange(station, operationPlan);
