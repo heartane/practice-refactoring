14870311e3578a93650755fcc67342baefb6c3e8
Jin garam <2001garam@naver.com>
2022-07-16 10:17:39 +0900

    [Refact] 변수 캡슐화하기 - 얕은 복사

--- refactoring2/ch6/6-6-use.js
+++ refactoring2/ch6/6-6-use.js
@@ -1,6 +1,9 @@
 import { getDefaultOwner } from './6-6.js';
 
+/* 
+참조값을 변경하면 원본 또한 변한다.
+*/
 const owner = getDefaultOwner();
-owner.firstName = '엘리';
+owner.firstName = '프레지덩';
 console.log(owner);
 console.log(getDefaultOwner());
