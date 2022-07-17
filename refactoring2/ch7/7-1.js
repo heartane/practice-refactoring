9bee0bf8b46198faa90f61539cca29ea9239c8d2
Jin garam <2001garam@naver.com>
2022-07-17 19:38:47 +0900

    [Refact] 레코드 캡슐화하기

--- refactoring2/ch7/7-1.js
+++ refactoring2/ch7/7-1.js
@@ -1,5 +1,39 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+/* 
+레코드 캡슐화하기
 
-organization.name = 'Dream Coding';
+read-only일지 write도 허용할지를 명시한 청사진을 만든다.
+쓰기 허용에서도 구체적인 로직을 통해 감시할 수 있다!
+*/
+export class Organization {
+  #name;
+  #country;
+  constructor(data) {
+    this.#name = data.name;
+    this.#country = data.country;
+  }
+  get name() {
+    return this.#name;
+  }
+  set name(value) {
+    this.#name = value;
+  }
+  get country() {
+    return this.#country;
+  }
+  get rawData() {
+    // 요구사항과 상황에 따라 달라질 수 있다. 깊은 복사 필요
+    return {
+      name: this.#name,
+      country: this.#country,
+    };
+  }
+}
+const organization = new Organization({
+  name: 'Acme Gooseberries',
+  country: 'GB',
+});
+
+organization.name = '고라파덕';
 console.log(organization.name);
 console.log(organization.country);
+console.log(organization.rawData);
