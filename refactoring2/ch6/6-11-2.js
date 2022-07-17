82c226c2b5c6c4ef6b1227e3d90761fb0c8dfff1
Jin garam <2001garam@naver.com>
2022-07-17 12:31:38 +0900

    [Refact] 스크립팅 작업 단계 쪼개기

--- refactoring2/ch6/6-11-2.js
+++ refactoring2/ch6/6-11-2.js
@@ -1,18 +1,43 @@
 import fs from 'fs';
+/* 
+스크립팅 관련 작업 단계 쪼개기
 
-if (!process.argv[2]) {
-  throw new Error('파일 이름을 입력하세요');
+testable을 고려해야 한다.
+process 객체에 직접적으로 접근을 하는 형식은 추후 테스트를 어렵게 한다.
+
+1. run 함수를 만들어서 노드의 process 의존성을 제거한다.
+2. 사용자에게 입력을 받는 단계 -> 유효성 검사
+3. 필요 로직 처리
+*/
+
+run(process.argv);
+
+function run(args) {
+  const command = parseCommand(args);
+  countOrders(command);
 }
 
-const fileName = `./${process.argv[2]}.json`;
-if (!fs.existsSync(fileName)) {
-  throw new Error('파일이 존재하지 않습니다');
+function parseCommand(args) {
+  if (!args[2]) {
+    throw new Error('파일 이름을 입력하세요');
+  }
+
+  const fileName = `./${args[2]}.json`;
+  if (!fs.existsSync(fileName)) {
+    throw new Error('파일이 존재하지 않습니다');
+  }
+
+  return {
+    fileName,
+    countReadyOnly: args.includes('-r'),
+  };
 }
 
-const rawData = fs.readFileSync(fileName);
-const orders = JSON.parse(rawData);
-if (process.argv.includes('-r')) {
-  console.log(orders.filter((order) => order.status === 'ready').length);
-} else {
-  console.log(orders.length);
+function countOrders({ fileName, countReadyOnly }) {
+  const rawData = fs.readFileSync(fileName);
+  const orders = JSON.parse(rawData);
+  const filtered = countReadyOnly
+    ? orders.filter((order) => order.status === 'ready')
+    : orders;
+  console.log(filtered.length);
 }
