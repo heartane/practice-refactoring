6f330fd6e7109d511d7c5cf1bb953b810484e9e3
Jin garam <2001garam@naver.com>
2022-07-18 09:22:17 +0900

    [Refact] 임시변수 질의함수로 바꾸기

--- refactoring2/ch7/7-4.js
+++ refactoring2/ch7/7-4.js
@@ -1,3 +1,11 @@
+/* 
+임수 변수를 질의 함수로 바꾸기
+
+간결하고 명확하게 함수의 역할이 잘 작성되어있어도, 
+재사용이 필요한 부분이 있다면 추출을 고려한다.
+각각의 변수들에 할당되었던 것들의 활용도가 더 높아진다.
+*/
+
 class Order {
   #quantity;
   #item;
@@ -5,11 +13,13 @@ class Order {
     this.#quantity = quantity;
     this.#item = item;
   }
-
+  get basePrice() {
+    return this.#quantity * this.#item.price;
+  }
+  get discountFactor() {
+    return this.basePrice > 1000 ? 0.95 : 0.98;
+  }
   get price() {
-    const basePrice = this.#quantity * this.#item.price;
-    const discountFactor = 0.98;
-    if (basePrice > 1000) discountFactor -= 0.03;
-    return basePrice * discountFactor;
+    return this.basePrice * this.discountFactor;
   }
 }
