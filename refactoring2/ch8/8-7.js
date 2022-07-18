b97165a0c254de6300577e068974d078c8649ada
Jin garam <2001garam@naver.com>
2022-07-18 10:59:13 +0900

    [Refact] 반복문 쪼개기

--- refactoring2/ch8/8-7.js
+++ refactoring2/ch8/8-7.js
@@ -1,10 +1,24 @@
-export function reportYoungestAgeAndTotalSalary(people) {
-  let youngest = people[0] ? people[0].age : Infinity;
-  let totalSalary = 0;
-  for (const p of people) {
-    if (p.age < youngest) youngest = p.age;
-    totalSalary += p.salary;
-  }
+/* 
+반복문 쪼개기
 
-  return `youngestAge: ${youngest}, totalSalary: ${totalSalary}`;
+성능은 아직 하지마라, 그리고 측정 후에 문제가 될 때 하라.
+*/
+
+export function reportYoungestAgeAndTotalSalary(people) {
+  return `youngestAge: ${youngestAge()}, totalSalary: ${totalSalary()}`;
+
+  function youngestAge() {
+    let youngest = people[0] ? people[0].age : Infinity;
+    for (const p of people) {
+      if (p.age < youngest) youngest = p.age;
+    }
+    return youngest;
+  }
+  function totalSalary() {
+    let totalSalary = 0;
+    for (const p of people) {
+      totalSalary += p.salary;
+    }
+    return totalSalary;
+  }
 }
