dc4b8b30979c7e0d44bef6d47086eb12b6169e4e
Jin garam <2001garam@naver.com>
2022-07-18 11:51:08 +0900

    [Refact] 반복문 쪼개기 - 알고리즘 교체

--- refactoring2/ch8/8-7.js
+++ refactoring2/ch8/8-7.js
@@ -2,23 +2,16 @@
 반복문 쪼개기
 
 성능은 아직 하지마라, 그리고 측정 후에 문제가 될 때 하라.
+더 나은 코드를 위해 계속 매진하고 축적해서 적용하자
 */
 
 export function reportYoungestAgeAndTotalSalary(people) {
   return `youngestAge: ${youngestAge()}, totalSalary: ${totalSalary()}`;
 
   function youngestAge() {
-    let youngest = people[0] ? people[0].age : Infinity;
-    for (const p of people) {
-      if (p.age < youngest) youngest = p.age;
-    }
-    return youngest;
+    return Math.min(...people.map((p) => p.age));
   }
   function totalSalary() {
-    let totalSalary = 0;
-    for (const p of people) {
-      totalSalary += p.salary;
-    }
-    return totalSalary;
+    return people.reduce((sum, p) => (sum += p.salary), 0);
   }
 }
