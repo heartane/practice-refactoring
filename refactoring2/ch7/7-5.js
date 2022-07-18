2d0e12c6841f483bdfd1620f94160e8e94e196a8
Jin garam <2001garam@naver.com>
2022-07-18 09:23:18 +0900

    [Refact] 클래스 추출하기

--- refactoring2/ch7/7-5.js
+++ refactoring2/ch7/7-5.js
@@ -1,44 +1,65 @@
+/* 
+클래스 추출하기
+
+클래스는 하나의 책임, 역할, 도메인을 가져야 한다.
+여러 행동이 섞여있다면, 역할 분담을 위해 추출을 고민하자.
+*/
 class Person {
   #name;
-  #officeAreaCode;
-  #officeNumber;
-  constructor(name, areaCode, number) {
+  #phoneNumber;
+  constructor(name, phoneNumber) {
     this.#name = name;
-    this.#officeAreaCode = areaCode;
-    this.#officeNumber = number;
+    this.#phoneNumber = phoneNumber;
   }
 
   get name() {
     return this.#name;
   }
-
   set name(arg) {
     this.#name = arg;
   }
-
-  get telephoneNumber() {
-    return `(${this.officeAreaCode}) ${this.officeNumber}`;
+  get phoneNumber() {
+    return this.#phoneNumber.toString;
   }
-
   get officeAreaCode() {
-    return this.#officeAreaCode;
+    return this.#phoneNumber.areaCode;
   }
-
-  set officeAreaCode(arg) {
-    this.#officeAreaCode = arg;
-  }
-
   get officeNumber() {
-    return this.#officeNumber;
-  }
-
-  set officeNumber(arg) {
-    this.#officeNumber = arg;
+    return this.#phoneNumber.number;
   }
 }
 
-const person = new Person('엘리', '010', '12345678');
+export class TelephoneNumber {
+  #areaCode;
+  #number;
+  constructor(areaCode, number) {
+    this.#areaCode = areaCode;
+    this.#number = number;
+  }
+  get toString() {
+    return `(${this.areaCode}) ${this.number}`;
+  }
+
+  get areaCode() {
+    return this.#areaCode;
+  }
+
+  set areaCode(arg) {
+    this.#areaCode = arg;
+  }
+
+  get number() {
+    return this.#number;
+  }
+
+  set number(arg) {
+    this.#number = arg;
+  }
+}
+
+const phoneNumber = new TelephoneNumber('010', '12345678');
+const person = new Person('문', phoneNumber);
 console.log(person.name);
+console.log(person.phoneNumber);
 console.log(person.officeAreaCode);
 console.log(person.officeNumber);
-console.log(person.telephoneNumber);
