cc74f44e335b8f01b881bfd7342901c8bd1038f8
Jin garam <2001garam@naver.com>
2022-07-19 10:31:11 +0900

    [Refact] 문장 슬라이드하기

--- refactoring2/ch8/8-6.js
+++ refactoring2/ch8/8-6.js
@@ -1,18 +1,21 @@
+/* 
+문장 슬라이드하기
+
+사용하는 것과 가까이 위치하는 것이 좋다. 
+그래야 코드를 읽어나갈 때 흐름이 끊기지 않는다.
+*/
+
 // 예제 1
 const pricingPlan = retrievePricingPlan();
+const chargePerUnit = pricingPlan.unit;
 const order = retreiveOrder();
 let charge;
-const chargePerUnit = pricingPlan.unit;
 
 // 예제 2
 function someFunc() {
-  let result;
-  if (availableResources.length === 0) {
-    result = createResource();
-    allocatedResources.push(result);
-  } else {
-    result = availableResources.pop();
-    allocatedResources.push(result);
-  }
+  const result = availableResources.length
+    ? availableResources.pop()
+    : createResource();
+  allocatedResources.push(result);
   return result;
 }
