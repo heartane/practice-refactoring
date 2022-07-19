9d8f67325bdd607c6b68db6aeb31f9aa3755b10e
Jin garam <2001garam@naver.com>
2022-07-19 10:27:59 +0900

    [Refact] 문장을 호출한 곳으로 옮기기 - 변형해서 사용

--- refactoring2/ch8/8-4.js
+++ refactoring2/ch8/8-4.js
@@ -1,3 +1,10 @@
+/* 
+문장을 호출한 곳으로 옮기기
+
+공통적으로 재사용되는 코드라면, 함수로 묶어서 재사용성을 높이고
+호출하는 곳마다 조금 다르게 사용하는 부분이 있다면 그 부분은 추출해서
+사용하는 곳에서 변형해서 사용할 수 있게 한다. 혹은 콜백함수로 받아올 수도 있다.
+*/
 function renderPerson(outStream, person) {
   outStream.write(`<p>${person.name}</p>\n`);
   renderPhoto(outStream, person.photo);
@@ -6,10 +13,10 @@ function renderPerson(outStream, person) {
 
 function listRecentPhotos(outStream, photos) {
   photos
-    .filter((p) => p.date > recentDateCutoff())
-    .forEach((p) => {
+    .filter((photo) => photo.date > recentDateCutoff())
+    .forEach((photo) => {
       outStream.write('<div>\n');
-      emitPhotoData(outStream, p);
+      emitPhotoData(outStream, photo);
       outStream.write('</div>\n');
     });
 }
