1960cb85b16a93afb8b00f8c1a964ac2e4e5d8ca
Jin garam <2001garam@naver.com>
2022-07-20 11:00:21 +0900

    [Refact] 파생 변수를 질의 함수로 바꾸기

--- refactoring2/ch9/9-3.js
+++ refactoring2/ch9/9-3.js
@@ -1,13 +1,18 @@
+/* 
+파생 변수를 질의 함수로 바꾸기
+
+뭔가 변할 수 있는 데이터를 가지고 있어서 다른 로직에 영향을 줄 수 있는 상태라면,
+수동적으로 일일이 수정하지말고, 필요한 경우 실시간으로 바로 데이터를 계산할 수 있도록 질의 함수를 만든다.
+*/
+
 // 예제 1
 class Order {
   // 다른 코드 있다고 가정
   get discountedTotal() {
-    return this._discountedTotal;
+    return this._basePrice - this._discount;
   }
   set discount(value) {
-    const old = this._discount;
     this._discount = value;
-    this._discountedTotal += old - value;
   }
 }
 
@@ -15,10 +20,9 @@ class Order {
 class ProductionPlan {
   // 다른 코드 있다고 가정
   get production() {
-    return this._production;
+    return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
   }
   applyAdjustment(adjustment) {
     this._adjustments.push(adjustment);
-    this._production += adjustment.amount;
   }
 }
