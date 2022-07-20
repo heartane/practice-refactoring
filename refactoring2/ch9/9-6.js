11c95e2fdffc94646d01403864adb5a058696754
Jin garam <2001garam@naver.com>
2022-07-20 11:01:15 +0900

    [Refact] 매직 리터럴 바꾸기

--- refactoring2/ch9/9-6.js
+++ refactoring2/ch9/9-6.js
@@ -1,3 +1,10 @@
+/* 
+매직 리터럴 바꾸기
+
+추측하기 어렵게 하는 숫자는 상수로 정의해서 의미를 부여한다.
+*/
+const STANDARD_GRAVITY = 9.81;
+
 function potentialEnergy(mass, height) {
-  return mass * 9.81 * height;
+  return mass * STANDARD_GRAVITY * height;
 }
