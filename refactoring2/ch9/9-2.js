461c2fe932f882e45665279b627b49ad5eccce66
Jin garam <2001garam@naver.com>
2022-07-20 10:58:49 +0900

    [Refact] 필드 이름 바꾸기

--- refactoring2/ch9/9-2.js
+++ refactoring2/ch9/9-2.js
@@ -1,12 +1,17 @@
+/* 
+필드 이름 바꾸기
+
+필드(속성)명은 클래스를 대표하는 타이틀이 되기때문에 매우 중요하다! 
+*/
 class Organization {
   constructor(data) {
     this._name = data.name;
     this._country = data.country;
   }
-  get name() {
+  get title() {
     return this._name;
   }
-  set name(value) {
+  set title(value) {
     this._name = value;
   }
   get country() {
@@ -17,6 +22,6 @@ class Organization {
   }
 }
 const organization = new Organization({
-  name: '드림코딩',
+  name: '삼성',
   country: '대한민국',
 });
