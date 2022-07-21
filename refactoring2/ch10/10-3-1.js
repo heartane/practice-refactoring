9f33dc08fbdff48c01675507f3e5c4abec9c662d
Jin garam <2001garam@naver.com>
2022-07-21 14:50:13 +0900

    [Refact] 중첩 조건문을 보호 구문으로 바꾸기

--- refactoring2/ch10/10-3-1.js
+++ refactoring2/ch10/10-3-1.js
@@ -1,20 +1,24 @@
+/* 
+중첩 조건문을 보호 구문으로 바꾸기
+
+쉽게 결과가 도출될 수 있는 부분은 우선적으로 반환해준다.
+그리고 보호 구문을 지나 들어온 주요 로직들을 가장 하단에서 처리함으로 조건문 중첩을 해결할 수 있다.
+즉, 내가 원하는 조건에서 벗어나는 것들을 먼저 빨리 나가도록(early exit) 하는 구조가 좋다.
+*/
+
 export function payAmount(employee) {
-  let result;
   if (employee.isSeparated) {
-    result = { amount: 0, reasonCode: 'SEP' };
-  } else {
-    if (employee.isRetired) {
-      result = { amount: 0, reasonCode: 'RET' };
-    } else {
-      // lorem.ipsum(dolor.sitAmet);
-      // consectetur(adipiscing).elit();
-      // sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-      // ut.enim.ad(minim.veniam);
-      result = someFinalComputation();
-      return result;
-    }
+    return { amount: 0, reasonCode: 'SEP' };
   }
-  return result;
+
+  if (employee.isRetired) {
+    return { amount: 0, reasonCode: 'RET' };
+  }
+  // lorem.ipsum(dolor.sitAmet);
+  // consectetur(adipiscing).elit();
+  // sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+  // ut.enim.ad(minim.veniam);
+  return someFinalComputation();
 }
 
 function someFinalComputation() {
