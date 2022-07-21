97748535b402226c84f5a42a6e30eabb21070eb7
Jin garam <2001garam@naver.com>
2022-07-21 14:53:17 +0900

    [Refact] 조건부 로직을 다형성으로 바꾸기

--- refactoring2/ch10/10-4-1.js
+++ refactoring2/ch10/10-4-1.js
@@ -1,32 +1,80 @@
+/* 
+조건부 로직을 다형성으로 바꾸기
+
+switch문은 새로운 것이 추가될 때마다 조건을 추가하면서 하염없이 길어진다.
+
+이를 클래스로 추상화하고 상속을 통한 다형성을 이용하면 쉽게 공통의 속성의 고유한 값을 얻을 수 있다.
+또 새로운 요구사항이 생겼을 때는 쉽게 인스턴스를 만들고 추가할 수 있다.
+만약 이 중 수정사항이 생기면 해당 클래스에서 쉽게 유지보수할 수 있다.
+*/
+
+// abstract class는 아니지만, 제일 최상위 부모 클래스
+class Bird {
+  #name;
+  constructor(name) {
+    this.#name = name;
+  }
+  get name() {
+    return this.#name;
+  }
+
+  get plumage() {
+    return 'unknown';
+  }
+
+  get airSpeedVelocity() {
+    return null;
+  }
+}
+
+class EuropeanSwallow extends Bird {
+  constructor() {
+    super('EuropeanSwallow');
+  }
+
+  get plumage() {
+    return 'average';
+  }
+
+  get airSpeedVelocity() {
+    return 35;
+  }
+}
+
+class AfricanSwallow extends Bird {
+  constructor() {
+    super('AfricanSwallow');
+  }
+
+  get plumage() {
+    return this.numberOfCoconuts > 2 ? 'tired' : 'average';
+  }
+
+  get airSpeedVelocity() {
+    return 40 - 2 * this.numberOfCoconuts;
+  }
+}
+
+class NorwegianBlueParrot extends Bird {
+  constructor() {
+    super('NorwegianBlueParrot');
+  }
+
+  get plumage() {
+    return this.voltage > 100 ? 'scorched' : 'beautiful';
+  }
+
+  get airSpeedVelocity() {
+    return this.isNailed ? 0 : 10 + this.voltage / 10;
+  }
+}
+
 export function plumages(birds) {
-  let map = birds.map((b) => [b.name, plumage(b)]);
-  let map1 = new Map(map);
-  return map1;
+  return new Map(birds.map((b) => [b.name, b.plumage]));
 }
 export function speeds(birds) {
-  return new Map(birds.map((b) => [b.name, airSpeedVelocity(b)]));
+  return new Map(birds.map((b) => [b.name, b.airSpeedVelocity]));
 }
-export function plumage(bird) {
-  switch (bird.type) {
-    case 'EuropeanSwallow':
-      return 'average';
-    case 'AfricanSwallow':
-      return bird.numberOfCoconuts > 2 ? 'tired' : 'average';
-    case 'NorwegianBlueParrot':
-      return bird.voltage > 100 ? 'scorched' : 'beautiful';
-    default:
-      return 'unknown';
-  }
-}
-export function airSpeedVelocity(bird) {
-  switch (bird.type) {
-    case 'EuropeanSwallow':
-      return 35;
-    case 'AfricanSwallow':
-      return 40 - 2 * bird.numberOfCoconuts;
-    case 'NorwegianBlueParrot':
-      return bird.isNailed ? 0 : 10 + bird.voltage / 10;
-    default:
-      return null;
-  }
-}
+
+const result = plumages([new NorwegianBlueParrot(), new AfricanSwallow()]);
+console.log(result);
--- refactoring2/ch10/10-4-2.js
+++ refactoring2/ch10/10-4-2.js
@@ -1,50 +1,77 @@
+/* 
+조건부 로직을 다형성으로 바꾸기2
+
+상속은 새, 동물, 사람 등 현실에 존재하는 물질뿐만 아니라
+점수, 등급 등 현존하지 않는 부분도 클래스로 만들어 서로 공통된 것은 하나의 추상화된 클래스에 묶고
+다른 부분은 따로 빼서 오버라이딩을 구현하면 조건문을 굳이 사용하지 않아도 다형성만으로 깔끔하게 로직을 구성할 수 있다.
+*/
+
 export function rating(voyage, history) {
   // 투자 등급
-  const vpf = voyageProfitFactor(voyage, history);
-  const vr = voyageRisk(voyage);
-  const chr = captainHistoryRisk(voyage, history);
-  if (vpf * 3 > vr + chr * 2) return 'A';
-  else return 'B';
-}
-
-function voyageRisk(voyage) {
-  // 항해 경로 위험요소
-  let result = 1;
-  if (voyage.length > 4) result += 2;
-  if (voyage.length > 8) result += voyage.length - 8;
-  if (['china', 'east-indies'].includes(voyage.zone)) result += 4;
-  return Math.max(result, 0);
-}
-
-function captainHistoryRisk(voyage, history) {
-  // 선장의 항해 이력 위험 요소
-  let result = 1;
-  if (history.length < 5) result += 4;
-  result += history.filter((v) => v.profit < 0).length;
-  if (voyage.zone === 'china' && hasChina(history)) result -= 2;
-  return Math.max(result, 0);
-}
-
-function hasChina(history) {
-  // 중국을 경유하는가?
-  return history.some((v) => 'china' === v.zone);
-}
-
-function voyageProfitFactor(voyage, history) {
-  // 수익 요인
-  let result = 2;
-  if (voyage.zone === 'china') result += 1;
-  if (voyage.zone === 'east-indies') result += 1;
-  if (voyage.zone === 'china' && hasChina(history)) {
-    result += 3;
-    if (history.length > 10) result += 1;
-    if (voyage.length > 12) result += 1;
-    if (voyage.length > 18) result -= 1;
-  } else {
-    if (history.length > 8) result += 1;
-    if (voyage.length > 14) result -= 1;
+  if (voyage.zone === 'china' && history.some((v) => 'china' === v.zone)) {
+    return new ChinaZoneRating(voyage, history).grade;
   }
-  return result;
+  return new Rating(voyage, history).grade;
+}
+class Rating {
+  constructor(voyage, history) {
+    this.voyage = voyage;
+    this.history = history;
+  }
+
+  get grade() {
+    const profit = this.voyageProfitFactor;
+    const risk = this.voyageRisk;
+    const historyRisk = this.captainHistoryRisk;
+    return profit * 3 > risk + historyRisk * 2 ? 'A' : 'B';
+  }
+
+  get voyageProfitFactor() {
+    // 수익 요인
+    let result = 2;
+    if (this.voyage.zone === 'china') result += 1;
+    if (this.voyage.zone === 'east-indies') result += 1;
+    result += this.voyageAndHistoryLengthFactor;
+    return result;
+  }
+
+  get voyageAndHistoryLengthFactor() {
+    let result = 0;
+    if (this.history.length > 8) result += 1;
+    if (this.voyage.length > 14) result -= 1;
+    return result;
+  }
+
+  get voyageRisk() {
+    // 항해 경로 위험요소
+    let result = 1;
+    if (this.voyage.length > 4) result += 2;
+    if (this.voyage.length > 8) result += this.voyage.length - 8;
+    if (['china', 'east-indies'].includes(this.voyage.zone)) result += 4;
+    return Math.max(result, 0);
+  }
+  get captainHistoryRisk() {
+    // 선장의 항해 이력 위험 요소
+    let result = 1;
+    if (this.history.length < 5) result += 4;
+    result += this.history.filter((v) => v.profit < 0).length;
+    return Math.max(result, 0);
+  }
+}
+
+class ChinaZoneRating extends Rating {
+  get captainHistoryRisk() {
+    const result = super.captainHistoryRisk - 2;
+    return Math.max(result, 0);
+  }
+
+  get voyageAndHistoryLengthFactor() {
+    let result = 3;
+    if (this.history.length > 10) result += 1;
+    if (this.voyage.length > 12) result += 1;
+    if (this.voyage.length > 18) result -= 1;
+    return result;
+  }
 }
 
 const voyage = { zone: 'west-indies', length: 10 };
