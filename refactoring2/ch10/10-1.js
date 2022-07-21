e71d1b366de1734884660661ba145e9eac4a0fcd
Jin garam <2001garam@naver.com>
2022-07-21 14:48:47 +0900

    [Refact] 조건문 분해하기

--- refactoring2/ch10/10-1.js
+++ refactoring2/ch10/10-1.js
@@ -1,7 +1,20 @@
+/* 
+조건문 분해하기
+
+조건문에서 연속적인 연산자와 부정의 연속은 이해를 어렵게 한다.
+따라서 표현식을 이해하기 쉽게 변수나 질의 함수로 추출해서 의미를 명확하게 표현하는 것이 좋다.
+*/
+
 function calculateCharge(date, quantity, plan) {
-  let charge = 0;
-  if (!date.isBefore(plan.summerStart) && !date.isAfter(plan.summerEnd))
-    charge = quantity * plan.summerRate;
-  else charge = quantity * plan.regularRate + plan.regularServiceCharge;
-  return charge;
+  return isSummer() ? calculateSummerCharge() : calculateRegularCharge();
+
+  function isSummer() {
+    return !date.isBefore(plan.summerStart) && !date.isAfter(plan.summerEnd);
+  }
+  function calculateSummerCharge() {
+    return quantity * plan.summerRate;
+  }
+  function calculateRegularCharge() {
+    return quantity * plan.regularRate + plan.regularServiceCharge;
+  }
 }
