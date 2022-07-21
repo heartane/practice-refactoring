a474c78a20947a35378c70e9c14fac36349ed165
Jin garam <2001garam@naver.com>
2022-07-21 14:49:25 +0900

    [Refact] 조건식 통합하기

--- refactoring2/ch10/10-2.js
+++ refactoring2/ch10/10-2.js
@@ -1,6 +1,18 @@
+/* 
+조건식 통합하기
+
+동일한 동작을 수행하는 다양한 조건이 있다면, 
+하나의 조건문으로 뭉쳐서 이름을 부여하고 사용하면 훨씬 코드가 깔끔해지고 의도를 잘 나타낼 수 있다.
+*/
+
 function disabilityAmount(employee) {
-  if (employee.seniority < 2) return 0;
-  if (employee.monthsDisabled > 12) return 0;
-  if (employee.isPartTime) return 0;
-  return 1;
+  return ineligibleForDisability(employee) ? 0 : 1;
+}
+
+function ineligibleForDisability(employee) {
+  return (
+    employee.seniority < 2 ||
+    employee.monthsDisabled > 12 ||
+    employee.isPartTime
+  );
 }
