d50948fb7f93e050c819a690166b6c45e64b3ccf
Jin garam <2001garam@naver.com>
2022-07-22 16:56:25 +0900

    [Refact] 세터 제거하기

--- refactoring2/ch11/11-7.js
+++ refactoring2/ch11/11-7.js
@@ -1,4 +1,10 @@
+/* 
+setter 제거하기
+
+클래스 설계 시 R/W에 대한 제어를 고려해야 한다.
+write는 항상 setter를 통하고, 이때 인자의 데이터 유효성 검사가 필요하다.
+*/
 class Person {
   get name() {}
-  set name(value) {}
+  // set name(value) {}
 }
