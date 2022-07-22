3c3e74b899fde09ca560a7fa1d98ee7a96fd3e31
Jin garam <2001garam@naver.com>
2022-07-22 16:59:28 +0900

    [Refact] 명령(커맨드 객체)를 함수로 바꾸기

--- refactoring2/ch11/11-10.js
+++ refactoring2/ch11/11-10.js
@@ -1,13 +1,14 @@
-export class ChargeCalculator {
-  constructor(customer, usage, provider) {
-    this._customer = customer;
-    this._usage = usage;
-    this._provider = provider;
-  }
-  get baseCharge() {
-    return this._customer.baseRate * this._usage;
-  }
-  get charge() {
-    return this.baseCharge + this._provider.connectionCharge;
-  }
+/* 
+명령을 함수로 바꾸기
+
+프로그램 많은 부분에서 특정 동작으로 재사용한다면, 필요 속성과 명령 기능을 묶어 커맨드 객체를 만드는 것은 좋지만
+단 한번만 사용한다면 그 인스턴스 생성은 불필요한 메모리 자원을 소모하는 셈이다. 따라서 함수로 사용하는 것이 좋다.
+
+유틸리티 함수를 만들 때 매번 인스턴스를 생성하지 않고 클래스의 static 함수를 이용하는 것도 
+불필요한 메모리 자원을 방지하기 위함이다.
+*/
+
+function charge(customer, usage, provider) {
+  const baseCharge = customer.baseRate * usage;
+  return baseCharge + provider.connectionCharge;
 }
