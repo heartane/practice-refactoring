72f8d6c9ebead3970264fc924bb222b95be3f6c5
Jin garam <2001garam@naver.com>
2022-07-22 16:52:25 +0900

    [Refact] 함수 매개변수화

--- refactoring2/ch11/11-2.js
+++ refactoring2/ch11/11-2.js
@@ -1,30 +1,27 @@
-// 예제 1
-function tenPercentRaise(person) {
-  person.salary = person.salary.multiply(1.1);
-}
+/* 
+함수 매개변수화하기 
 
-function fivePercentRaise(person) {
-  person.salary = person.salary.multiply(1.05);
+살짝 다른 부분으로 인해 공통의 함수로 추출하기 어렵다면, 그 부분은 매개변수로 전달하는 방법을 고려한다.
+이를 위해서는 로직에 대한 충분한 이해와 테스트 코드가 동반되어야 한다.
+*/
+
+// 예제 1
+function salaryRaise(person, factor) {
+  person.salary = person.salary.multiply(1 + factor);
 }
 
 // 예제 2
 export function baseCharge(usage) {
   if (usage < 0) return usd(0);
   const amount =
-    bottomBand(usage) * 0.03 + middleBand(usage) * 0.05 + topBand(usage) * 0.07;
+    withinBand(usage, 0, 100) * 0.03 +
+    withinBand(usage, 100, 200) * 0.05 +
+    withinBand(usage, 200, Infinity) * 0.07;
   return usd(amount);
 }
 
-function bottomBand(usage) {
-  return Math.min(usage, 100);
-}
-
-function middleBand(usage) {
-  return usage > 100 ? Math.min(usage, 200) - 100 : 0;
-}
-
-function topBand(usage) {
-  return usage > 200 ? usage - 200 : 0;
+function withinBand(usage, bottom, top) {
+  return usage > bottom ? Math.min(usage, top) - bottom : 0;
 }
 
 function usd(value) {
