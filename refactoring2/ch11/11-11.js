6f7eefce0deae25a440ba5d7bb317e033fc36b95
Jin garam <2001garam@naver.com>
2022-07-22 17:01:09 +0900

    [Refact] 수정된 값 반환하기

--- refactoring2/ch11/11-11.js
+++ refactoring2/ch11/11-11.js
@@ -1,13 +1,21 @@
-export function ascentVelocity(points, totalMinutes) {
-  function calculateAscent() {
-    for (let i = 1; i < points.length; i++) {
-      const verticalChange = points[i].elevation - points[i - 1].elevation;
-      totalAscent += verticalChange > 0 ? verticalChange : 0;
-    }
-  }
+/* 
+수정된 값 반환하기
 
-  let totalAscent = 0;
-  calculateAscent([{ elevation: 10 }, { elevation: 20 }]);
+함수가 외부의 상태를 변경하거나, 인자로 전달된 특정 객체를 직접적으로 변경하는 것은 사이드 이팩트를 야기한다.
+무엇을 반환하는 형식으로 값을 알려야 한다.
+*/
+
+export function ascentVelocity(points, totalMinutes) {
+  const totalAscent = calculateAscent(points);
 
   return totalAscent / totalMinutes;
+}
+
+function calculateAscent(points) {
+  let result;
+  for (let i = 1; i < points.length; i++) {
+    const verticalChange = points[i].elevation - points[i - 1].elevation;
+    result += verticalChange > 0 ? verticalChange : 0;
+  }
+  return result;
 }
