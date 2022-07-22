753a479fd712b85560235310681b8c96a60e75bb
Jin garam <2001garam@naver.com>
2022-07-22 16:54:08 +0900

    [Refact] 객체 통째로 넘기기

--- refactoring2/ch11/11-4.js
+++ refactoring2/ch11/11-4.js
@@ -1,8 +1,19 @@
+/* 
+객체 통째로 넘기기
+
+어느 정도 규모의 인자를 넘길 것인가? 매개변수 범위 규정하기의 정도는 없다.
+
+함수에서 특정한 인자를 전달받을 때, 이것이 객체에 쌓여져 있다면 그대로 전달하는 것이 실용적일 수 있다.
+그러나 그 함수가 객체 안의 일부분의 속성만 필요하다면 전체를 전달하는 것은 자칫 위험할 수 있다.
+
+따라서 객체를 통째로 전달하는 것은 간편할 수 있지만, 커플링이 불필요하게 높아질 수 있기 때문에
+전달받는 객체에 대한 의존성을 함수가 가져도 괜찮은지를 고려해야 한다.
+*/
+
 export function temperatureAlerts(room, plan) {
   const alerts = [];
-  const low = room.daysTempRange.low;
-  const high = room.daysTempRange.high;
-  if (!plan.withinRange(low, high)) {
+
+  if (!plan.withinRange(room.daysTempRange)) {
     alerts.push('room temperature went outside range');
   }
 
@@ -14,9 +25,10 @@ export class HeatingPlan {
     this._temperatureRange = temperatureRange;
   }
 
-  withinRange(bottom, top) {
+  withinRange(range) {
     return (
-      bottom >= this._temperatureRange.low && top <= this._temperatureRange.high
+      range.bottom >= this._temperatureRange.low &&
+      range.top <= this._temperatureRange.high
     );
   }
 }
