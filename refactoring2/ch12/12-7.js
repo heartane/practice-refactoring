40df75414cd443dfbc2b0c3ac281a7120f6ea9ea
Jin garam <2001garam@naver.com>
2022-07-23 12:46:49 +0900

    [Refact] 서브 클래스 제거하기

--- refactoring2/ch12/12-7.js
+++ refactoring2/ch12/12-7.js
@@ -1,7 +1,21 @@
+/* 
+서브클래스 제거하기
+
+서브 클래스가 필요없는 경우는 언제일까?
+서브 클래스마다 다형성을 이용해 다른 동작을 핸들링하는 유즈 케이스가 없고
+그저 속성 하나만 달라서 상속을 통해 하위 클래스를 만든거라면 오버 엔지니어링에 가깝다.
+이럴 때는 하나의 클래스에서 속성을 다르게 설정하는 것이 더 나을 수도 있다.
+
+외부에서 전혀 알 필요없는 세부 로직들은 잘 캡슐화하고, 
+생성에 편리하게 static 팩토리 메서드를 제공해준다.
+*/
+
 class Person {
   #name;
-  constructor(name) {
+  #genderCode;
+  constructor(name, genderCode) {
     this.#name = name;
+    this.#genderCode = genderCode;
   }
 
   get name() {
@@ -9,45 +23,36 @@ class Person {
   }
 
   get genderCode() {
-    return 'X';
+    return this.#genderCode;
   }
-}
 
-class Male extends Person {
-  get genderCode() {
-    return 'M';
+  get isMale() {
+    return this.#genderCode === 'M';
   }
-}
 
-class Female extends Person {
-  get genderCode() {
-    return 'F';
+  static create(record) {
+    switch (record.gender) {
+      case 'M':
+        return new Person(record.name, 'M');
+      case 'F':
+        return new Person(record.name, 'F');
+      default:
+        return new Person(record.name, 'X');
+    }
   }
 }
 
 function loadFromInput(data) {
   const result = [];
-  data.forEach((record) => {
-    let person;
-    switch (record.gender) {
-      case 'M':
-        person = new Male(record.name);
-        break;
-      case 'F':
-        person = new Female(record.name);
-        break;
-      default:
-        person = new Person(record.name);
-    }
-    result.push(person);
-  });
+  data.forEach((record) => result.push(Person.create(record)));
   return result;
 }
 
 const people = loadFromInput([
-  { name: '엘리', gender: 'F' },
+  { name: '영희', gender: 'F' },
   { name: '철수', gender: 'M' },
   { name: '밥', gender: 'M' },
 ]);
-const numberOfMales = people.filter((p) => p instanceof Male).length;
+
+const numberOfMales = people.filter((p) => p.isMale).length;
 console.log(numberOfMales);
