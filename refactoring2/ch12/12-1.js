88ebb98dd32109b51e1d6d626cca4935cfbbcc7c
Jin garam <2001garam@naver.com>
2022-07-23 12:42:53 +0900

    [Refact] 메서드 상위 클래스로 올리기

--- refactoring2/ch12/12-1.js
+++ refactoring2/ch12/12-1.js
@@ -1,24 +1,25 @@
+/* 
+메서드 올리기
+
+서브 클래스에 중복해서 사용된다면, 부모 클래스의 메서드로 끌어올려서 중복을 방지하자.
+코드 재사용성뿐만 아니라 해당 클래스의 공통된 규격을 한번에 파악할 수 있다.
+*/
+
 // 예시 1
-class Employee {}
-
-class Salesperson extends Employee {
+class Employee {
   get name() {}
 }
 
-class Engineer extends Employee {
-  get name() {}
-}
+class Salesperson extends Employee {}
+
+class Engineer extends Employee {}
 
 // 예시 2
-class Party {}
-
-class Department extends Party {
-  get totalAnnualCost() {
-    return this.monthlyCost * 12;
-  }
-}
-class Employee extends Party {
+class Party {
   get annualCost() {
     return this.monthlyCost * 12;
   }
 }
+
+class Department extends Party {}
+class Employee extends Party {}
