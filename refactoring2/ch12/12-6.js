c8debbe10a36d2290a833a90f5c842c268f6ef0d
Jin garam <2001garam@naver.com>
2022-07-23 12:45:43 +0900

    [Refact] 타입별 코드 서브 클래스로 바꾸기

--- refactoring2/ch12/12-6.js
+++ refactoring2/ch12/12-6.js
@@ -1,26 +1,59 @@
+/* 
+타입 코드를 서브클래스로 바꾸기
+
+생성자에서 에러를 던지는 것은 좋지 않고
+외부의 실수를 validate하는 것보다 실수를 하지 않도록 코드를 유도하는 것이 좋다.
+*/
+
 class Employee {
   #name;
-  #type;
-  constructor(name, type) {
-    this.validateType(type);
+  constructor(name) {
     this.#name = name;
-    this.#type = type;
-  }
-
-  validateType(arg) {
-    if (!['engineer', 'manager', 'salesperson'].includes(arg)) {
-      throw new Error(`${arg}라는 직원 유형은 없습니다.`);
-    }
   }
 
   get type() {
-    return this.#type;
+    return 'employee';
   }
 
-  toString() {
+  get toString() {
     return `${this.#name} (${this.type})`;
+  }
+
+  static createEmployee(name, type) {
+    switch (type) {
+      case 'engineer':
+        return new Engineer(name);
+      case 'manager':
+        return new Manager(name);
+      case 'salesperson':
+        return new Salesperson(name);
+      default:
+        throw new Error(`${type} 라는 직원 유형은 없습니다`);
+    }
   }
 }
 
-const ellie = new Employee('엘리', 'engineer');
-const bob = new Employee('밥', 'manager');
+class Engineer extends Employee {
+  get type() {
+    return 'engineer';
+  }
+}
+
+class Manager extends Employee {
+  get type() {
+    return 'manager';
+  }
+}
+
+class Salesperson extends Employee {
+  get type() {
+    return 'salesperson';
+  }
+}
+
+const employee = new Employee('람');
+const rami = new Engineer('라미');
+const bob = Employee.createEmployee('밥', 'manager');
+
+console.log(rami.toString);
+console.log(bob.toString);
